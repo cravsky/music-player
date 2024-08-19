cd9769b4dc172b6fb7af41a5531aa615c4c38ba8
cravsky <cravsky@gmail.com>
2024-08-19 15:48:58 +0200

    Displaying current time and duration

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -72,10 +76,34 @@ function nextSong() {
     playSong();
 }
 
-// Play or Pause Event Listener
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    const { duration, currentTime } = e.srcElement;
+    if (isPlaying) {
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    const durationSeconds = Math.floor(duration % 60).toString().padStart(2, '0');
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds !== 'NaN') {
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for currentTime
+    const currentTimeMinutes = Math.floor(currentTime / 60);
+    const currentTimeSeconds = Math.floor(currentTime % 60).toString().padStart(2, '0');
+    // Delay switching duration Element to avoid NaN
+    if (currentTimeSeconds !== 'NaN') {
+        currentTimeEl.textContent = `${currentTimeMinutes}:${currentTimeSeconds}`;
+    }
+}
+
+// Event Listeners
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 prevBtn.addEventListener('click', () => prevSong());
 nextBtn.addEventListener('click', () => nextSong());
+music.addEventListener('timeupdate', updateProgressBar);
 
 
 // Update DOM
